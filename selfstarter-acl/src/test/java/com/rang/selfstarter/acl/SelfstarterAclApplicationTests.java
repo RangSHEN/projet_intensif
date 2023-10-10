97c415f4f747ba02a2b9312cbc4327c33f6d7c3f
RangSHEN <alexrang007@gmail.com>
2023-10-10 11:54:59 +0200

    projet initial

---
+++ selfstarter-acl/src/test/java/com/rang/selfstarter/acl/SelfstarterAclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rang.selfstarter.acl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfstarterAclApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
