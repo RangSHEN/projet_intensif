c1be205885875a1ac701d5b0d4016560651a3fbd
RangSHEN <Alex1995!>
2023-10-10 12:18:31 +0200

    sdd

--- selfstarter-acl/src/main/java/com/rang/selfstarter/SelfstarterAclApplication.java
+++ selfstarter-acl/src/main/java/com/rang/selfstarter/SelfstarterAclApplication.java
@@ -1,4 +1,4 @@
-package com.rang.selfstarter.acl;
+package com.rang.selfstarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
